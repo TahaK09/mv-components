19d2171eb72f50efe85267feb9cd8893adadd8b6
TahaK09 <tahafirozkhan39@gmail.com>
2025-07-01 00:07:33 +0530

    Commit on July, 1st

--- client/src/components/moreSection.jsx
+++ client/src/components/moreSection.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import mvplus from "../assets/meavana_plus.png";
+import mvblogs from "../assets/meavana_blogs.jpg";
+import mvschools from "../assets/meavana_schools.jpg";
 
 function MoreSection() {
   return (
     <>
       <section className="px-6 py-12 bg-white">
         <h2 className="text-4xl font-extrabold text-[#282828] text-center my-20">
-          More from the MeaVana
+          More from MeaVana
         </h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-7xl mx-auto">
-          <div className="bg-green-100 rounded-lg p-7 hover:shadow-lg cursor-pointer">
-            <h3 className="font-extrabold mb-1  text-xl text-green-700">
+          <div className="bg-green-100  flex flex-col justify-center items-center rounded-lg  py-7 px-16 shadow-lg cursor-pointer">
+            <h3 className="font-extrabold mb-1 text-xl text-green-700">
               MeaVana Blog
             </h3>
             <p className="text-green-600 text-sm">
-              Explore our blog for the latest insights, tips, and stories.
+              Find out what we are up to!
             </p>
+            <img
+              src={mvblogs}
+              alt="meavana plus"
+              className="w-64 h-auto mt-3.5 shadow-2xl rounded-lg"
+            />
           </div>
-          <div className="bg-violet-100 flex flex-col justify-center items-center rounded-lg p-7 hover:shadow-lg cursor-pointer">
+          <div className="bg-violet-100 flex flex-col justify-center items-center rounded-lg  py-7 px-16 shadow-lg cursor-pointer">
             <h3 className="mb-1 text-xl font-bold text-violet-700">
               MeaVana Plus
             </h3>
@@ -27,17 +34,21 @@ function MoreSection() {
             <img
               src={mvplus}
               alt="meavana plus"
-              className="w-80 h-auto mt-2 shadow-xl"
+              className="w-64 h-auto mt-3.5 shadow-2xl rounded-lg"
             />
           </div>
-          <div className="bg-yellow-100 rounded-lg p-7 hover:shadow-lg cursor-pointer">
-            <h3 className="font-extrabold mb-1  text-xl text-yellow-700">
-              Resources
+          <div className="bg-[#efdec3] flex flex-col justify-center items-center rounded-lg py-7 px-16 shadow-lg cursor-pointer">
+            <h3 className="font-extrabold mb-1 text-xl text-[#745f3e]">
+              MeaVana For Schools
             </h3>
-            <p className="text-yellow-600 text-sm">
-              Access a variety of resources including articles, videos, and
-              guides to help you on your mental health journey.
+            <p className="text-[#745f3e] text-sm">
+              Empower students with MeaVana.
             </p>
+            <img
+              src={mvschools}
+              alt="meavana plus"
+              className="w-64 h-auto mt-3.5 shadow-2xl rounded-lg"
+            />
           </div>
         </div>
       </section>
