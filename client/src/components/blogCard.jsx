31e58e4ada96c5172ab1218e8740c2c6f423f5d1
TahaK09 <tahafirozkhan39@gmail.com>
2025-07-03 00:33:16 +0530

    Blog component completed

--- client/src/components/blogCard.jsx
+++ client/src/components/blogCard.jsx
@@ -1,24 +1,27 @@
 import React from "react";
+import TimeSvg from "../assets/icons8-time.svg";
 
-function BlogCard() {
+function BlogCard({ Title, Description, Time, Image }) {
   return (
-    <div className="mt-20 max-w-96 rounded-xl overflow-hidden shadow-lg bg-white transition-shadow duration-200 hover:shadow-2xl">
-      <div className="overflow-hidden max-h-[200px]">
+    <div className="cursor-pointer mt-20 max-w-84 rounded-xl overflow-hidden shadow-lg bg-white transition-shadow duration-200 hover:shadow-2xl">
+      <div className="overflow-hidden max-h-[160px]">
         <img
-          src="https://images.unsplash.com/photo-1506744038136-46273834b3fb?auto=format&fit=crop&w=400&q=80"
+          src={Image}
           alt="Blog"
           className="w-full h-full object-cover transition-transform duration-400 ease-[cubic-bezier(.4,2,.6,.8)] group-hover:scale-105"
         />
       </div>
       <div className="p-3">
-        <h2 className="text-lg mb-1 font-bold text-[#222]">Blog Title</h2>
+        <h2 className="text-lg/6 mb-2 font-bold text-[#222]">{Title}</h2>
         <p className="text-[#555] text-sm">
-          This is a short description of the blog post. It gives a quick
-          overview of the content.
+          {Description.length > 100
+            ? `${Description.slice(0, 100)}...`
+            : Description}
         </p>
-      </div>
-      <div className="p-3">
-        <p className="text-[#1f1f1f] text-sm">2 mins read</p>
+        <div className="mt-3 text-[#363636] text-sm flex items-center gap-1.5">
+          <img src={TimeSvg} alt="Time" className="w-4 h-4 rotate-315" />
+          <span>{Time}</span>
+        </div>
       </div>
     </div>
   );
