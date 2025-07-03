7d5ed8a550a49de66aabffb443f3199c49e45c25
TahaK09 <tahafirozkhan39@gmail.com>
2025-07-04 02:15:53 +0530

    Path for Vercel Fixed 2

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./app.jsx";
-import { AppContextProvider } from "./context/AppContext.jsx";
+import { AppContextProvider } from "./Context/AppContext.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
