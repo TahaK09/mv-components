a9f13ef94be590cdfd708fdc737c8f7b10f015bc
TahaK09 <tahafirozkhan39@gmail.com>
2025-09-04 14:04:49 +0530

    Added Animation to the componenen

--- client/src/components/animationFeatureCard.jsx
+++ client/src/components/animationFeatureCard.jsx
@@ -9,7 +9,7 @@ function AnimationFeatureCard() {
     <>
       <div className="w-screen h-screen bg-white flex justify-center items-center gap-10">
         {/* Left Side Text */}
-        <div className="w-96 space-y-4">
+        <div className="w-96 space-y-4 fade-in-up">
           <div className="font-semibold text-2xl">StickyNotes</div>
           <div className="font-medium text-base text-gray-600">
             Create a deep work routine with sounds, timers and focus-enhancing
@@ -22,22 +22,22 @@ function AnimationFeatureCard() {
           <img
             src={img1}
             alt="Image 1"
-            className="w-32 h-32 absolute top-0 left-8"
+            className="w-32 h-32 absolute top-5 left-6 fade-in-up fade-delay-2"
           />
           <img
             src={img2}
             alt="Image 2"
-            className="w-36 h-36 absolute top-12 right-0"
+            className="w-36 h-36 absolute top-12 right-0 fade-in-left fade-delay-2"
           />
           <img
             src={img3}
             alt="Image 3"
-            className="w-60 h-60 absolute top-24 left-20 z-10 transition-all duration-300 opacity-0 transform hover:top-20 hover:opacity-100"
+            className="w-60 h-60 absolute top-24 left-20 z-10 fade-in-up"
           />
           <img
             src={img4}
             alt="Image 4"
-            className="w-32 h-32 absolute bottom-0 right-8"
+            className="w-32 h-32 absolute bottom-0 right-8 fade-in-down fade-delay-2"
           />
         </div>
       </div>
